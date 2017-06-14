b07c56e6edea013dd0e6fe45826e5b939e901503
Vianney Foucault <vianney.foucault@gmail.com>
2017-06-14 21:35:00 +0200

    Wip : New provider

--- lib/puppet/provider/kafka_topic/confluent.rb
+++ lib/puppet/provider/kafka_topic/confluent.rb
@@ -1,43 +1,101 @@
-require 'pp'
+require 'socket'
+require 'timeout'
+
 Puppet::Type.type(:kafka_topic).provide(:confluent) do
+  # Without initvars commands won't work.
+  initvars
 
-  has_feature :manage_topics
-  commands :kafka_topics => '/usr/bin/kafka-topics'
+  commands :kafka_topics => '/usr/bin/kafka_topics'
 
-  mk_resource_methods
-
-  @@topics_classvars = {
-      :zk_host => "",
-      :topics => "",
-      :initialized => false,
-  }
-
-  def initialize(value={})
-    super(value)
-    @property_flush = {}
-    if not @@topics_classvars[:initialized] then
-      output = `grep -E '(server.1|clientPort)' /etc/kafka/zookeeper.properties`.chomp.split("\n")
-      zk_info = output.map { |x| x.split('=')}.map { |x| { x[0].strip.gsub('.','').to_sym => x[1].strip.gsub(%r{:\d*},'') }}.reduce Hash.new, :merge
-      ignored_topics = %w[__confluent.support.metrics _schemas]
-      @@topics_classvars[:zk_host] = zk_info[:server1] + ':' + zk_info[:clientPort]
-      topics = kafka_topics('--list', '--zookeeper', @@topics_classvars[:zk_host]).chomp.split("\n")
-      @@topics_classvars[:topics] = topics - ignored_topics
-      @@topics_classvars[:initialized] = true
-      Puppet.debug "Known Kafka Topics : #{topics}"
+  # fetch zkhosts
+  def self.get_zk_host
+    if File.file?("/etc/kafka/zookeeper.properties")
+      rawdata = File.open("/etc/kafka/server.properties").readlines().flat_map {|x| x.strip.split('=')[1].split(',') if x.strip.start_with?('zookeeper.connect=')}.compact
+      alive = rawdata.map {|x| x if port_open?(x)}.compact
+      if alive.length > 0
+        return alive.sample
+      else
+        raise Puppet::Error "No zookeeper host alive"
+      end
+    else
+      raise Puppet::Error "Unable to discover zookeeper host"
     end
   end
 
-  TOPIC_REGEX = %r{Topic:(?'topicname'[\S]*).*PartitionCount:(?'numpartition'\d{1,2}).*ReplicationFactor:(?'rep_factor'\d{1,2})}
 
-  def self.describe_topic(name)
-    output = kafka_topics('--describe', '--topic', name, '--zookeeper', @@topics_classvars[:zk_host]).chomp
-    topic_info = output.match(TOPIC_REGEX)
-    {ensure: :present,
-     name: topic_info['topicname'],
-     num_partitions: topic_info['numpartition'],
-     replication_factor: topic_info['rep_factor'],
-     provider: :confluent}
+  def port_open?(uri, seconds=1)
+    Timeout::timeout(seconds) do
+      begin
+        host, port = uri.split(":")
+        TCPSocket.new(host, port).close
+        true
+      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+        false
+      end
+    end
+  rescue Timeout::Error
+    false
   end
+
+
+  def get_zk_host
+    self.class.get_zk_host
+  end
+
+  def self.topics
+    kafka_topics('--list', '--zookeeper', get_zk_host).chomp.split("\n")
+  end
+
+  # Optional parameter to run a statement on the MySQL system database.
+  def self.describe_topics
+    hashdata = Hash.new
+    output = kafka_topics('--describe', '--zookeeper', get_zk_host).chomp.split("\n")
+    output.split('Topic:').map {|x| x.strip if !x.empty?}.compact.each do |entry|
+      splitted = entry.split("\t")
+      if splitted.length > 1
+        if splitted[1].start_with?('PartitionCount:')
+          num_partition = splitted[1].split(':')[1]
+          replication = splitted[2].split(':')[1]
+          hashdata[splitted[0]] = {:name => splitted[0], :num_partition => num_partition, :replication => replication, :partitions => []}
+        elsif splitted[1].start_with?('Partition:')
+          # topic1", "Partition: 0", "Leader: 2", "Replicas: 2,1,3", "Isr: 2,1,3"
+          partition = splitted[1].split()[1]
+          leader = splitted[2].split()[1]
+          replicats = splitted[3].split()[1]
+          insync = splitted[4].split()[1]
+          hashstatus = {:partition => partition, :leader => leader, :replicats => replicats, :isr => insync}
+          hashdata[splitted[0]][:partitions].push(hashstatus)
+        end
+      else
+        next
+      end
+    end
+    retarray = []
+    hashdata.each do |topic, data|
+      retarray.push(data)
+    end
+    return retarray
+  end
+
+  def self.instances
+    self.describe_topics do |instance|
+      new(:name => instance[:name],
+          :ensure => :present,
+          :num_partition => instance[:num_partition],
+          :replication_factor => instance[:replication]
+      )
+    end
+  end
+
+  def self.prefetch(resources)
+    topics = instances
+    resources.keys.each do |topic|
+      if provider = topics.find {|db| db.name == topic}
+        resources[topic].provider = provider
+      end
+    end
+  end
+
 
   def create
     @property_flush[:ensure] = :present
@@ -45,41 +103,22 @@ Puppet::Type.type(:kafka_topic).provide(:confluent) do
   end
 
   def exists?
-    @@topics_classvars[:topics].include?(resource[:name])
+    @property_hash[:ensure] == :present || false
   end
 
   def destroy
     @property_flush[:ensure] = :absent
-
-  end
-
-  def self.prefetch(resources)
-    instances.each do |prov|
-      if resources['prov'] = resources[prov.name]
-        resources['prov'].provider = prov
-      end
-    end
-
-  end
-
-  def self.instances
-    puts @@topics_classvars
-    existing_topics = []
-    @@topics_classvars[:topics].each do |topic|
-      existing_topics << new(describe_topic(topic))
-    end
-    existing_topics
   end
 
 
   def do_the_job
     case @property_flush[:ensure]
       when :absent
-        kafka_topics('--delete', '--force', '--topic', resource[:name], '--zookeeper', @@topics_classvars[:zk_host])
+        kafka_topics('--delete', '--force', '--topic', resource[:name], '--zookeeper', get_zk_host)
       when :present
         kafka_topics('--create',
                      '--topic', resource[:name],
-                     '--zookeeper', @@topics_classvars[:zk_host],
+                     '--zookeeper', get_zk_host,
                      '--partitions', resource[:num_partitions],
                      '--replication-factor', resource[:replication_factor])
     end
@@ -90,12 +129,4 @@ Puppet::Type.type(:kafka_topic).provide(:confluent) do
     @property_hash = self.class.describe_topic(resource[:name])
   end
 
-
-  def correct_params?(hash_topic)
-    resource_hash = {name: resource[:name],
-                     num_partitions: resource[:num_partitions],
-                     replication_factor: resource[:replication_factor],
-                     zookeeper: resource[:zookeeper]}
-    raise ArgumentError, ('Can\t update topic for now. Current settings != new settings') unless hash_topic == resource_hash
-  end
 end
--- spec/unit/provider/kafka_topic/confluent_spec.rb
+++ spec/unit/provider/kafka_topic/confluent_spec.rb
@@ -1,122 +1,122 @@
-require 'spec_helper'
-provider_class = Puppet::Type.type(:kafka_topic).provider(:confluent)
-
-context 'installing vendor-modulename' do
-
-  let(:list_topics) do
-    <<-OUTPUT
-  topic1
-  topic2
-  TestTopic
-    OUTPUT
-  end
-  let(:list_topics_details_topic1) do
-    <<-OUTPUT
-  Topic:topic1	PartitionCount:3	ReplicationFactor:3	Configs:
-  	Topic: topic1	Partition: 0	Leader: 1	Replicas: 1,3,2	Isr: 1,3,2
-  	Topic: topic1	Partition: 1	Leader: 2	Replicas: 2,1,3	Isr: 2,1,3
-  	Topic: topic1	Partition: 2	Leader: 3	Replicas: 3,2,1	Isr: 3,2,1
-    OUTPUT
-  end
-  let(:list_topics_details_topic2) do
-    <<-OUTPUT
-  Topic:topic2	PartitionCount:2	ReplicationFactor:2	Configs:
-  	Topic: topic2	Partition: 0	Leader: 1	Replicas: 1,2 	Isr: 1,2
-  	Topic: topic2	Partition: 1	Leader: 2	Replicas: 2,1 	Isr: 2,1
-    OUTPUT
-  end
-  let(:list_topics_details_TestTopic) do
-    <<-OUTPUT
-  Topic:TestTopic	PartitionCount:1	ReplicationFactor:1	Configs:
-  	Topic: TestTopic	Partition: 0	Leader: 1	Replicas: 1 	Isr: 1
-    OUTPUT
-  end
-
-  let(:get_zk_host) do
-    <<-OUTPUT
-  clientPort=2181
-  server.1 = zookeeper1.corp:2188:2888
-    OUTPUT
-  end
-
-  describe provider_class do
-    #include PuppetSpec::Fixtures
-
-    let(:resource) do
-      Puppet::Type.type(:kafka_topic).new(
-          :name => 'TestTopic2',
-          :ensure => :present,
-          :provider => 'confluent'
-      )
-    end
-
-
-    let(:provider) do
-
-      provider = provider_class.new
-      provider.stubs(:`).with('grep -E \'(server.1|clientPort)\' /etc/kafka/zookeeper.properties').returns(:get_zk_host)
-      provider.stubs(:kafka_topic).with('--list', '--zookeeper', 'zookeeper1.corp:2181').returns(:list_topics)
-      provider.resource = resource
-      provider
-    end
-
-    before :each do
-      resource.provider = provider
-    end
-
-    describe 'provider features' do
-      it { is_expected.to be_versionable }
-      it { is_expected.to be_install_options }
-      [:install, :latest, :update, :install_options].each do |method|
-        it "should have a(n) #{method}" do
-          is_expected.to respond_to(method)
-        end
-      end
-    end
-
-    # describe 'when installing' do
-    #   it 'should use the path to puppet with arguments' do
-    #     provider_class.stubs(:command).with(:puppetcmd).returns "/my/puppet"
-    #     provider.expects(:execute).with {|args| args.join(' ') == "/my/puppet module install vendor-modulename" }.returns ""
-    #     provider.install
-    #   end
-    # end
-
-  end
-
-end
 # require 'spec_helper'
-# require 'pp'
+# provider_class = Puppet::Type.type(:kafka_topic).provider(:confluent)
 #
-# confluent_provider = Puppet::Type.type(:kafka_topic).provider(:confluent)
+# context 'installing vendor-modulename' do
 #
-# describe confluent_provider do
-#   # it_behaves_like 'a kafka_topic provider', confluent_provider
-#
-
-#
-#     context 'the whole init' do
-#
-#       describe 'instances' do
-#         it 'should have an instance method' do
-#           expect(described_class).to respond_to :instances
-#         end
-#       end
-#
-#       describe 'prefetch' do
-#         it 'should have a prefetch method' do
-#           expect(described_class).to respond_to :prefetch
-#         end
-#       end
-#     end
-#
-#   context 'get topics' do
-#     before :each do
-
-#     end
-#     it 'should return no resources' do
-#       expect(described_class.instances.size).to eq(0)
-#     end
+#   let(:list_topics) do
+#     <<-OUTPUT
+#   topic1
+#   topic2
+#   TestTopic
+#     OUTPUT
 #   end
-# end
+#   let(:list_topics_details_topic1) do
+#     <<-OUTPUT
+#   Topic:topic1	PartitionCount:3	ReplicationFactor:3	Configs:
+#   	Topic: topic1	Partition: 0	Leader: 1	Replicas: 1,3,2	Isr: 1,3,2
+#   	Topic: topic1	Partition: 1	Leader: 2	Replicas: 2,1,3	Isr: 2,1,3
+#   	Topic: topic1	Partition: 2	Leader: 3	Replicas: 3,2,1	Isr: 3,2,1
+#     OUTPUT
+#   end
+#   let(:list_topics_details_topic2) do
+#     <<-OUTPUT
+#   Topic:topic2	PartitionCount:2	ReplicationFactor:2	Configs:
+#   	Topic: topic2	Partition: 0	Leader: 1	Replicas: 1,2 	Isr: 1,2
+#   	Topic: topic2	Partition: 1	Leader: 2	Replicas: 2,1 	Isr: 2,1
+#     OUTPUT
+#   end
+#   let(:list_topics_details_TestTopic) do
+#     <<-OUTPUT
+#   Topic:TestTopic	PartitionCount:1	ReplicationFactor:1	Configs:
+#   	Topic: TestTopic	Partition: 0	Leader: 1	Replicas: 1 	Isr: 1
+#     OUTPUT
+#   end
 #
+#   let(:get_zk_host) do
+#     <<-OUTPUT
+#   clientPort=2181
+#   server.1 = zookeeper1.corp:2188:2888
+#     OUTPUT
+#   end
+#
+#   describe provider_class do
+#     #include PuppetSpec::Fixtures
+#
+#     let(:resource) do
+#       Puppet::Type.type(:kafka_topic).new(
+#           :name => 'TestTopic2',
+#           :ensure => :present,
+#           :provider => 'confluent'
+#       )
+#     end
+#
+#
+#     let(:provider) do
+#
+#       provider = provider_class.new
+#       provider.stubs(:`).with('grep -E \'(server.1|clientPort)\' /etc/kafka/zookeeper.properties').returns(:get_zk_host)
+#       provider.stubs(:kafka_topic).with('--list', '--zookeeper', 'zookeeper1.corp:2181').returns(:list_topics)
+#       provider.resource = resource
+#       provider
+#     end
+#
+#     before :each do
+#       resource.provider = provider
+#     end
+#
+#     describe 'provider features' do
+#       it { is_expected.to be_versionable }
+#       it { is_expected.to be_install_options }
+#       [:install, :latest, :update, :install_options].each do |method|
+#         it "should have a(n) #{method}" do
+#           is_expected.to respond_to(method)
+#         end
+#       end
+#     end
+#
+#     # describe 'when installing' do
+#     #   it 'should use the path to puppet with arguments' do
+#     #     provider_class.stubs(:command).with(:puppetcmd).returns "/my/puppet"
+#     #     provider.expects(:execute).with {|args| args.join(' ') == "/my/puppet module install vendor-modulename" }.returns ""
+#     #     provider.install
+#     #   end
+#     # end
+#
+#   end
+#
+# end
+# # require 'spec_helper'
+# # require 'pp'
+# #
+# # confluent_provider = Puppet::Type.type(:kafka_topic).provider(:confluent)
+# #
+# # describe confluent_provider do
+# #   # it_behaves_like 'a kafka_topic provider', confluent_provider
+# #
+#
+# #
+# #     context 'the whole init' do
+# #
+# #       describe 'instances' do
+# #         it 'should have an instance method' do
+# #           expect(described_class).to respond_to :instances
+# #         end
+# #       end
+# #
+# #       describe 'prefetch' do
+# #         it 'should have a prefetch method' do
+# #           expect(described_class).to respond_to :prefetch
+# #         end
+# #       end
+# #     end
+# #
+# #   context 'get topics' do
+# #     before :each do
+#
+# #     end
+# #     it 'should return no resources' do
+# #       expect(described_class.instances.size).to eq(0)
+# #     end
+# #   end
+# # end
+# #
